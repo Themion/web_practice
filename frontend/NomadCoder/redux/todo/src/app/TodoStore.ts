e103118b96f1d37e032e76fbf00ee7b5526085f9
themion7 <themion@naver.com>
2022-05-12 10:34:20 +0900

    todo 삭제 기능 추가

--- frontend/NomadCoder/redux/todo/src/app/TodoStore.ts
+++ frontend/NomadCoder/redux/todo/src/app/TodoStore.ts
@@ -1,24 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+export interface Todo {
+    id: number
+    text: string
+}
+
 interface TodoAction {
     type: string
-    todo: string
+    todo: Todo
 }
 
 export const ADD_TODO = "ADD_TODO";
 export const DELETE_TODO = "DELETE_TODO";
 
-const reducer = (state: string[] = [], action: TodoAction) => {
-    console.log(action);
+const reducer = (state: Todo[] = [], action: TodoAction) => {
     switch (action.type) {
         case ADD_TODO:
-            return state.concat([action.todo]);
+            return [ action.todo, ...state ]
         case DELETE_TODO:
-            return state.filter(item => item !== action.todo);
+            return state.filter(item => item.id !== action.todo.id);
         default:
             return state;
     }
 };
 
 export const todoStore = configureStore({ reducer: reducer });
-
--- frontend/NomadCoder/redux/todo/src/index.tsx
+++ frontend/NomadCoder/redux/todo/src/index.tsx
@@ -1,19 +1,40 @@
-import { ADD_TODO, todoStore } from "./app/TodoStore";
+import { ADD_TODO, DELETE_TODO, Todo, todoStore } from "./app/TodoStore";
 
 const form = document.querySelector("form") as HTMLFormElement;
 const input = document.querySelector("input") as HTMLInputElement;
 const ul = document.querySelector("ul") as HTMLUListElement;
 
+const dispatchAddTodo = (todo: Todo) => {
+    todoStore.dispatch({type: ADD_TODO, todo: todo})
+}
+const dispatchDeleteTodo = (todo: Todo) => {
+    todoStore.dispatch({type: DELETE_TODO, todo: todo}) 
+}
+
 const onChange = () => {
     ul.innerHTML = ""
     todoStore.getState().forEach(todo => {
-        ul.innerHTML += `<li>${todo}</li>`
+        const onClick = () => { dispatchDeleteTodo(todo) }
+        const span = document.createElement('span')
+        span.onclick = onClick
+        span.innerText = '❌'
+
+        const li = document.createElement('li')
+        li.id = todo.id.toString();
+        li.innerText = todo.text
+        li.appendChild(span)
+
+        ul.appendChild(li)
     })
 }
+
 const onSubmit = (e: SubmitEvent) => {
     e.preventDefault()
-    if (input.value === "") return
-    todoStore.dispatch({type: ADD_TODO, todo: input.value}) 
+    if (input.value !== "") dispatchAddTodo({
+        id: new Date().getTime(),
+        text: input.value
+    })
+
     input.value = ""
 }
 
