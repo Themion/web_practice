100f97383b4d483ab598e038464a1c1df4312507
themion7 <themion@naver.com>
2022-05-13 11:53:54 +0900

    createReducer 적용

--- frontend/NomadCoder/redux/todo/src/app/TodoStore.ts
+++ frontend/NomadCoder/redux/todo/src/app/TodoStore.ts
@@ -1,4 +1,4 @@
-import { configureStore, createAction } from "@reduxjs/toolkit";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
 
 export interface TodoType {
     id: number
@@ -11,23 +11,17 @@ export interface TodoAction {
 }
 
 export const addTodo = createAction('ADD', (text: string) => {
-    return {
-        payload: { id: new Date().getTime(), text } as TodoType
-    }
+    return { payload: { id: new Date().getTime(), text } as TodoType }
 })
 export const deleteTodo = createAction('DELETE', (id: number) => {
     return { payload: {id, text: ""} as TodoType }
 })
 
-const reducer = (state: TodoType[] = [], action: TodoAction) => {
-    switch (action.type) {
-        case addTodo.type:
-            return [ action.payload, ...state ]
-        case deleteTodo.type:
-            return state.filter(item => item.id !== action.payload.id);
-        default:
-            return state;
-    }
-};
+const reducer = createReducer([] as TodoType[], {
+    [addTodo.type]: (state: TodoType[], action: TodoAction) => 
+        [action.payload, ...state],
+    [deleteTodo.type]: (state: TodoType[], action: TodoAction) => 
+        state.filter(item => item.id !== action.payload.id)
+})
 
 export const todoStore = configureStore({ reducer: reducer });
