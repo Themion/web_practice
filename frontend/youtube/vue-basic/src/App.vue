b4b7adfb2a92de0792959987b345a332da24485f
themion7 <themion@naver.com>
2022-06-28 14:37:40 +0900

    vue - methods, event handler

--- frontend/youtube/vue-basic/src/App.vue
+++ frontend/youtube/vue-basic/src/App.vue
@@ -4,26 +4,33 @@
         <a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
     </div>
 
-    <img alt="Vue logo" src="./assets/logo.png">
     <div v-for="(product, i) in products" :key="i">
         <h4 :style="style">{{ i + 1 }}: {{ product.name }}</h4>
         <p>{{ product.price }}만원</p>
+        <button @click="report(product)">허위매물 신고</button>
+        <span @mouseover="log">신고 수: {{ product.report }}</span>
     </div>
+
 </template>
 
 <script>
+
 export default {
     name: 'App',
     data() {
         return {
             products: [
-                {name: '역삼동원룸', price: 60}, 
-                {name: '천호동원룸', price: 70}, 
-                {name: '마포구원룸', price: 55}, 
+                { name: '역삼동원룸', price: 60, report: 0 }, 
+                { name: '천호동원룸', price: 70, report: 0 }, 
+                { name: '마포구원룸', price: 55, report: 0 }, 
             ],
             menus: ['Home', 'Shop', 'About'],
             style: 'color: blue;',
         }  
+    },
+    methods: {
+        report(product) { product.report++ },
+        log() { console.log('log') },
     },
     components: {
     }
