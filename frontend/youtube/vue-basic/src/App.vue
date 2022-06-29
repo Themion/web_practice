3fb9d8539cfa6e246e60497f79b836c9596b9472
themion7 <themion@naver.com>
2022-06-29 10:22:01 +0900

    vue - modal

--- frontend/youtube/vue-basic/src/App.vue
+++ frontend/youtube/vue-basic/src/App.vue
@@ -1,17 +1,26 @@
-<template>
+<template><div>
+
+    <div class="black-bg" v-if="modal">
+        <div class="white-bg">
+            <h4>상세 페이지</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia quibusdam iste voluptatem, tempora unde commodi a praesentium minima doloribus delectus quam at dolorem vel voluptatum nemo sapiente hic voluptate tempore.</p>
+            <button class="white-bg btn" @click="modal = false">X</button>
+        </div>
+    </div>
 
     <div class="menu">
         <a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
     </div>
 
     <div v-for="(product, i) in products" :key="i">
+        <img class="room-img" :src="product.img" @click="modal = true">
         <h4 :style="style">{{ i + 1 }}: {{ product.name }}</h4>
         <p>{{ product.price }}만원</p>
         <button @click="report(product)">허위매물 신고</button>
         <span @mouseover="log">신고 수: {{ product.report }}</span>
     </div>
 
-</template>
+</div></template>
 
 <script>
 
@@ -20,12 +29,13 @@ export default {
     data() {
         return {
             products: [
-                { name: '역삼동원룸', price: 60, report: 0 }, 
-                { name: '천호동원룸', price: 70, report: 0 }, 
-                { name: '마포구원룸', price: 55, report: 0 }, 
+                { name: '역삼동원룸', price: 60, report: 0, img: require('@/assets/room/room0.jpg') }, 
+                { name: '천호동원룸', price: 70, report: 0, img: require('@/assets/room/room1.jpg') }, 
+                { name: '마포구원룸', price: 55, report: 0, img: require('@/assets/room/room2.jpg') }, 
             ],
             menus: ['Home', 'Shop', 'About'],
             style: 'color: blue;',
+            modal: false,
         }  
     },
     methods: {
@@ -38,6 +48,15 @@ export default {
 </script>
 
 <style>
+
+body {
+    margin: 0;
+}
+
+div {
+    box-sizing: border-box;
+}
+
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -55,6 +74,35 @@ export default {
 .menu a {
     color: white;
     padding: 10px;
+}
+
+.room-img {
+    width: 100%;
+    margin-top: 40px;
+}
+
+.black-bg {
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    padding: 20px;
+}
+
+.white-bg {
+    width: 100%;
+    background: white;
+    position: relative;
+    border-radius: 8px;
+    padding: 20px;
+}
+
+button.btn {
+    border: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: auto;
 }
 
 </style>
