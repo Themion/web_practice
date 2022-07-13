338708c238033fa87ff78d3039a8d3d8d262ff6e
themion7 <themion@naver.com>
2022-07-13 20:44:54 +0900

    3-4. v-for로 반복문 사용하기

--- frontend/inflearn/vue-webgame/3-number-baseball/src/App.vue
+++ frontend/inflearn/vue-webgame/3-number-baseball/src/App.vue
@@ -1,11 +1,15 @@
 <template>
-    <div>
-        <h1>{{result}}</h1>
-        <form v-on:submit="onSubmit">
-            <input type="text" maxlength="4" v-model="value" ref="answer">
-        </form>
-        <div>시도: {{time}}</div>
-    </div>
+    <h1>{{result}}</h1>
+    <form @submit.prevent="onSubmit">
+        <input type="text" maxlength="4" v-model="value" ref="answer">
+    </form>
+    <div>시도: {{tries.length}}</div>
+    <ul>
+        <li v-for="t in tries">
+            <div>{{t.try}}</div>
+            <div>{{t.result}}</div>
+        </li>
+    </ul>
 </template>
 
 <script>
@@ -14,12 +18,22 @@
             return {
                 value: '',
                 result: '',
-                time: 0,
+                tries: [],
             }
         },
         methods: {
+            setValue() {
+                this.value = ''
+                for (let i = 0; i < 4; i++) 
+                    this.value += `${Math.floor(Math.random() * 10)}`
+            },
             onSubmit(e) {
-                e.preventDefault();
+                this.tries.push({
+                    try: this.value,
+                    result: 'homerun'
+                })
+                this.value = ''
+                this.$refs.answer.focus()
             }
         }
     }
