2214d8056d918ac10a9c7e6559e33d0ba7ee7407
themion7 <themion@naver.com>
2022-07-14 13:34:05 +0900

    4-1. 프로젝트 생성

---
+++ frontend/inflearn/vue-webgame/4-response-time-check/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            init() {
+                
+            }
+        }
+    }
+</script>
+
+<template>
+    <div id="screen">{{}}</div>
+    <div>
+        <div>평균 시간: {{}}</div>
+        <button @click="init">리셋</button>
+    </div>
+</template>
+
+<style>
+</style>
