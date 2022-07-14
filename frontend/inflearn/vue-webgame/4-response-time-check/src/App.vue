afb504f56375f33fc2269677865042cea4001412
themion7 <themion@naver.com>
2022-07-14 13:59:33 +0900

    4-2. v-bind와 vue의 style

--- frontend/inflearn/vue-webgame/4-response-time-check/src/App.vue
+++ frontend/inflearn/vue-webgame/4-response-time-check/src/App.vue
@@ -1,25 +1,47 @@
 <script>
-    export default {
-        data() {
-            return {
-                
-            }
-        },
-        methods: {
-            init() {
-                
-            }
-        }
+export default {
+  data() {
+    return {
+      result: [],
+      state: 'waiting',
+      message: '클릭해서 시작하세요'
     }
+  },
+  methods: {
+    init() {
+
+    }
+  }
+}
 </script>
 
 <template>
-    <div id="screen">{{}}</div>
-    <div>
-        <div>평균 시간: {{}}</div>
-        <button @click="init">리셋</button>
-    </div>
+  <div id="screen" :class="state">{{ message }}</div>
+  <!-- <div id="screen" v-bind:class="state">{{message}}</div> -->
+  <div>
+    <div>평균 시간: {{}}</div>
+    <button @click="init">리셋</button>
+  </div>
 </template>
 
-<style>
+<style scoped>
+#screen {
+  width: 300px;
+  height: 200px;
+  text-align: center;
+  user-select: none;
+}
+
+#screen.waiting {
+  background-color: aqua;
+}
+
+#screen.ready {
+  background-color: red;
+  color: white;
+}
+
+#screen.bow {
+  background-color: greenyellow;
+}
 </style>
