4444d51ee675ff74529c11859ec595bea7420798
themion7 <themion@naver.com>
2022-07-16 18:08:00 +0900

    5-1. 프로젝트 생성

---
+++ frontend/inflearn/vue-webgame/5-rock-scissor-paper/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    data() {
+
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+
+}
+</script>
+
+<template>
+</template>
+
+<style scoped>
+#computer {
+    width: 142px;
+    height: 20px;
+    background-position: 0 0;
+}
+</style>
