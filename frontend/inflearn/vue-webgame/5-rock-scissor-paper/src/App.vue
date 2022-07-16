86de08ef71c212e18293f9982398bc0a85aa9200
themion7 <themion@naver.com>
2022-07-16 19:18:24 +0900

    5-2. :style

--- frontend/inflearn/vue-webgame/5-rock-scissor-paper/src/App.vue
+++ frontend/inflearn/vue-webgame/5-rock-scissor-paper/src/App.vue
@@ -1,25 +1,51 @@
 <script>
+const ROCK = '바위'
+const SCISSOR = '가위'
+const PAPER = '보'
+const imgPos = {
+  [ROCK]: '0',
+  [SCISSOR]: '-142px',
+  [PAPER]: '-284px'
+}
+
 export default {
-    data() {
-
-    },
-    computed: {
-
-    },
-    methods: {
-
-    },
+  data() {
+    return {
+      myState: ROCK,
+      cpuState: ROCK,
+      result: '',
+      score: 0,
+    }
+  },
+  computed: {
+    ImgUrl() {
+      return {
+        background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgPos[this.cpuState]} 0`
+      }
+    }
+  },
+  methods: {
+    setMyState(val) { this.myState = val }
+  },
 
 }
 </script>
 
 <template>
+  <div id="computer" :style="ImgUrl"></div>
+  <div>
+    <button @click="setMyState(SCISSOR)">가위</button>
+    <button @click="setMyState(ROCK)">바위</button>
+    <button @click="setMyState(PAPER)">보</button>
+  </div>
+  <div>{{result}}</div>
+  <div>현재 점수: {{score}}</div>
 </template>
 
 <style scoped>
 #computer {
-    width: 142px;
-    height: 20px;
-    background-position: 0 0;
+  width: 142px;
+  height: 200px;
+  background-position: 0 0;
 }
 </style>
