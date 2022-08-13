e244b32cc73949427584c12b3e077f4d2c21ff30
themion7 <themion@naver.com>
2022-08-13 22:27:23 +0900

    ref vs 일반 변수

--- frontend/youtube/react-hook/useRef/src/App.jsx
+++ frontend/youtube/react-hook/useRef/src/App.jsx
@@ -1,21 +1,30 @@
 import { useRef, useState } from "react"
 
 function App() {
-    const [count, setCount] = useState(0)
+    let count = 0
     const countRef = useRef(0)
-
-    console.log(countRef)
-
-    const increaseState = () => setCount(count + 1)
+    const [renderer, render] = useState(0)
+    
+    const increaseLet = () => {
+        count += 1
+        console.log('let: ' + count)
+    }
     // 자주 바뀌되 re-render될 필요 없는 값을 저장
-    const increaseRef = () => countRef.current += 1
+    // 일반 변수와 다르게 re-render 되더라도 값이 사라지지 않음
+    const increaseRef = () => {
+        countRef.current += 1
+        console.log('ref: ' + countRef.current)
+    }
+
+    const doRender = () => render(renderer + 1)
     
     return (
         <div className="App">
-            <p>State: {count}</p>
+            <p>let: {count}</p>
             <p>Ref: {countRef.current}</p>
-            <button onClick={increaseState}>State +</button>
+            <button onClick={increaseLet}>let +</button>
             <button onClick={increaseRef}>Ref +</button>
+            <button onClick={doRender}>render</button>
         </div>
     )
 }
