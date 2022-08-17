3d77579746128cba1b030f59b15fd690f6280506
themion7 <themion@naver.com>
2022-08-17 14:25:34 +0900

    의존성 배열은 객체를 값이 아닌 참조형으로 계산

--- frontend/youtube/react-hook/useMemo/src/App.jsx
+++ frontend/youtube/react-hook/useMemo/src/App.jsx
@@ -1,46 +1,31 @@
-import { useMemo, useState } from 'react'
-
-const hardCalculate = (number) => {
-    for (let i = 0; i < 999999999; i++) {}
-    return number + 10000
-}
-
-const easyCalculate = (number) => number + 1
+import { useEffect, useMemo, useState } from 'react'
 
 function App() {
-    const [hardNumber, setHardNumber] = useState(1)
-    const [easyNumber, setEasyNumber] = useState(1)
+    const [number, setNumber] = useState(0)
+    const [isKorea, setIsKorea] = useState(true)
 
-    const hardSum = useMemo(
-        () => hardCalculate(hardNumber),
-        [hardNumber]
-    )
-    const easySum = useMemo(
-        () => easyCalculate(easyNumber),
-        [easyNumber]
-    )
+    const location = {
+        country: isKorea ? '한국' : '외국'
+    }
 
-    const onHardChange = (e) => setHardNumber(parseInt(e.target.value))
-    const onEasyChange = (e) => setEasyNumber(parseInt(e.target.value))
+    // 의존성 배열은 객체를 값이 아닌 참조형으로 계산
+    useEffect(() => {
+        console.log('useEffect 호출')
+    }, [location])
 
     return (
         <div className="App">
-            <div>
-                <h3>어려운 계산기</h3>
-                <input
-                    type="number"
-                    value={hardNumber}
-                    onChange={onHardChange} />
-                <span> + 10000 = {hardSum}</span>
-            </div>
-            <div>
-                <h3>쉬운 계산기</h3>
-                <input
-                    type="number"
-                    value={easyNumber}
-                    onChange={onEasyChange} />
-                <span> + 1 = {easySum}</span>
-            </div>
+            <h2>하루에 몇끼 먹어요?</h2>
+            <input 
+                type="number" 
+                value={number} 
+                onChange={(e) => setNumber(e.target.value)} />
+            <hr />
+            <h2>어느 나라에 있어요?</h2>
+            <p>나라: {location.country}</p>
+            <button onClick={e => setIsKorea(!isKorea)}>
+                비행기 타자
+            </button>
         </div>
     )
 }
