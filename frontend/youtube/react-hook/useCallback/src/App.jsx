69c6815f551b35b609c9cbb4dae7b19af9cca0e6
themion7 <themion@naver.com>
2022-08-18 19:00:12 +0900

    재렌더링 할 때마다 useEffect 호출시키지 않음

--- frontend/youtube/react-hook/useCallback/src/App.jsx
+++ frontend/youtube/react-hook/useCallback/src/App.jsx
@@ -3,7 +3,12 @@ import { useCallback, useEffect, useState } from 'react'
 function App() {
     const [number, setNumber] = useState(0)
 
-    const log = () => console.log('number: ' + number)
+    // 재렌더링 할 때마다 useEffect 호출시키지 않음 
+    // 캡처링 때문에 number는 0
+    const log = useCallback(
+        () => console.log('number: ' + number),
+        []
+    )
 
     useEffect(() => {
         console.log('function log has changed')
