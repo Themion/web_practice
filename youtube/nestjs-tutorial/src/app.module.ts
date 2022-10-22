17177c4d5b2dc168b5ecbcb663367a2a01a00d3a
Themion <themion@naver.com>
2022-10-22 23:59:52 +0900

    nestjs의 설정 모듈 사용

--- youtube/nestjs-tutorial/src/app.module.ts
+++ youtube/nestjs-tutorial/src/app.module.ts
@@ -3,6 +3,15 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
-@Module({ imports: [AuthModule, UserModule, BookmarkModule, PrismaModule] })
+@Module({
+    imports: [
+        AuthModule,
+        BookmarkModule,
+        ConfigModule.forRoot({ isGlobal: true }),
+        PrismaModule,
+        UserModule,
+    ]
+})
 export class AppModule { }
--- youtube/nestjs-tutorial/src/prisma/prisma.service.ts
+++ youtube/nestjs-tutorial/src/prisma/prisma.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
-import { config } from 'dotenv';
-
-config();
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
+    constructor(config: ConfigService) {
+        const DATABASE_URL = 'DATABASE_URL';
+
         super({
             datasources: {
                 db: {
-                    url: process.env.DATABASE_URL
+                    url: config.get(DATABASE_URL)
                 }
             }
         });
