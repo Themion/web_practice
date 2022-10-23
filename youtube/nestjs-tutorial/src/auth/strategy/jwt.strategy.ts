c81aa33308fdfd3cd9f1d32b529e8186b6f9166a
Themion <themion@naver.com>
2022-10-24 00:16:03 +0900

    DB와 연결하여 사용자 인가 구현

--- youtube/nestjs-tutorial/src/auth/strategy/jwt.strategy.ts
+++ youtube/nestjs-tutorial/src/auth/strategy/jwt.strategy.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-    constructor(config: ConfigService) {
+    constructor(
+        config: ConfigService,
+        private prisma: PrismaService
+    ) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey: config.get(JwtStrategy.JWT_SECRET)
@@ -14,8 +18,15 @@ class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
 
     private static readonly JWT_SECRET = 'JWT_SECRET';
 
-    validate(payload: object) {
-        return payload;
+    async validate(payload: { sub: number, email: string; }) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: payload.sub
+            }
+        });
+
+        delete user.hash;
+        return user;
     }
 }
 
