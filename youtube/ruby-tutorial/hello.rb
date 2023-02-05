5dd74c5c6b51602e683dfc07b5b05ec4a2b70d54
Themion <themion@naver.com>
2023-02-05 15:26:43 +0900

    input

--- youtube/ruby-tutorial/hello.rb
+++ youtube/ruby-tutorial/hello.rb
@@ -1,13 +1,3 @@
-puts 3 + 5
-puts 3 * 5
-puts 3 / 5
-puts 3.0 / 5.0
-
-puts ("3 + 5 = " + (3 + 5).to_s)
-puts 40.5.round()
-puts 40.3.round()
-puts 40.5.floor()
-puts 40.3.ceil()
-
-puts Math.sqrt(16)
-puts Math.log(2.71)
+print "Enter your name: "
+name = gets.chomp()
+puts "Hello, " + name + "! Nice to meet you!"
