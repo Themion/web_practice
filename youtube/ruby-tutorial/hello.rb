d2ad3e1a4e07730ba5ce844f80b82e1770ea77e3
Themion <themion@naver.com>
2023-02-06 18:29:14 +0900

    if-elsif-else

--- youtube/ruby-tutorial/hello.rb
+++ youtube/ruby-tutorial/hello.rb
@@ -1,6 +1,17 @@
 def sayHi(name = "EnterNameHere")
     puts "Hello, " + name.to_s + "!"
+    return 1, 2
 end
 
-sayHi()
-sayHi("Nill")
+a = sayHi()[1]
+a += sayHi("Nill")[0]
+
+if (a == 5)
+    puts "a is 5"
+elsif (a < 5)
+    puts "a is smaller than 5"
+else 
+    puts "a is bigger than 5"
+end 
+
+puts a ** 3
