3081781d1c5e8db4a5951dd5a68904492a42b0c7
Themion <themion@naver.com>
2023-02-06 14:40:24 +0900

    method

--- youtube/ruby-tutorial/hello.rb
+++ youtube/ruby-tutorial/hello.rb
@@ -1,14 +1,6 @@
-hash = { a: 1, b: 2 }
-hash[:c] = 3
-
-nums.each do |num|
-    val += num.to_f
+def sayHi(name = "EnterNameHere")
+    puts "Hello, " + name.to_s + "!"
 end
-text = "text"
 
-puts val
-hash[text] = 4
-
-puts hash
-puts hash[:text] == hash["text"]
-puts hash['c'] == nil
+sayHi()
+sayHi("Nill")
