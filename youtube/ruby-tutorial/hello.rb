4082e6ad357685724c3c918787b567745101f043
Themion <themion@naver.com>
2023-02-08 20:35:37 +0900

    writing file

--- youtube/ruby-tutorial/hello.rb
+++ youtube/ruby-tutorial/hello.rb
@@ -1,20 +1,21 @@
-File.open('./file.txt', "r") do |file|
-    puts file.readchar()
-    puts file.readchar()
-    puts file.readchar()
-    puts file.read()
-end
-
-puts
-
-File.open('./file.txt', "r") do |file|
-    for line in file.readlines()
-        puts line
-    end
-end
-
-puts
+file = File.open('./file.txt', 'w')
+file.write("File has overwritten!")
+file.close()
 
 file = File.open('./file.txt', 'r')
-puts file.read
-# file.close()
+puts file.read()
+file.close()
+
+file = File.open('./file.txt', 'a')
+file.write("\nHello Ruby!")
+file.close()
+
+file = File.open('./file.txt', 'r+')
+puts file.read()
+file.write("\nFile has re-overwritten!")
+puts file.read()
+file.close()
+
+file = File.open('./index.html', 'w')
+file.write('<h1>Hello HTML!</h1>')
+file.close()
