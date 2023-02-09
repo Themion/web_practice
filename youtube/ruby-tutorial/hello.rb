d9d4bffb6f9ccbcf7ba432f3fc393963162e5100
Themion <themion@naver.com>
2023-02-09 21:26:13 +0900

    module & require

--- youtube/ruby-tutorial/hello.rb
+++ youtube/ruby-tutorial/hello.rb
@@ -1,24 +1,3 @@
-class A
-    def foo()
-        return "foo"
-    end
-    def bar()
-        return "bar"
-    end
-end
-
-class B < A
-    def foo()
-        return super() + '!'
-    end
-    def baz()
-        return "baz"
-    end
-end
-
-a = A.new()
-b = B.new()
-
-puts a.foo()
-puts b.foo()
-puts b.baz()
+require "./module.rb"
+include FooBar
+puts FooBar.foo()
---
+++ youtube/ruby-tutorial/module.rb
@@ -0,0 +1,11 @@
+module FooBar
+    def foo()
+        return "foo"
+    end
+    def bar()
+        return "bar"
+    end
+end
+
+# include FooBar
+# puts FooBar.foo()
