df5cb3112beb99afd03a0d52820027c8d3841f08
Themion <themion@naver.com>
2023-02-09 19:35:56 +0900

    try-catch

--- youtube/ruby-tutorial/hello.rb
+++ youtube/ruby-tutorial/hello.rb
@@ -1,21 +1,26 @@
-file = File.open('./file.txt', 'w')
-file.write("File has overwritten!")
-file.close()
+begin
+    nums = [1, 2, 3, 4, 5]
+    key = "num"
+    puts nums[key]
+rescue TypeError => e
+    print "Type error:\t\t "
+    puts e
+rescue ZeroDivisionError => e
+    print "Division by zero error:\t "
+    puts e
+rescue
+    puts "unknown error"
+end
 
-file = File.open('./file.txt', 'r')
-puts file.read()
-file.close()
 
-file = File.open('./file.txt', 'a')
-file.write("\nHello Ruby!")
-file.close()
-
-file = File.open('./file.txt', 'r+')
-puts file.read()
-file.write("\nFile has re-overwritten!")
-puts file.read()
-file.close()
-
-file = File.open('./index.html', 'w')
-file.write('<h1>Hello HTML!</h1>')
-file.close()
+begin
+    num = 10 / 0
+rescue TypeError => e
+    print "Type error:\t\t "
+    puts e
+rescue ZeroDivisionError => e
+    print "Division by zero error:\t "
+    puts e
+rescue
+    puts "unknown error"
+end
