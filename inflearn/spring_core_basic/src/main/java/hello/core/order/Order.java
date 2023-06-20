9bd49f854139d84ff08155e56e3ea3ebfa7f1d2d
Themion <themion@naver.com>
2023-06-20 16:32:39 +0900

    스프링 핵심 원리 기본편 - 롬복과 최신 트렌드

--- inflearn/spring_core_basic/src/main/java/hello/core/order/Order.java
+++ inflearn/spring_core_basic/src/main/java/hello/core/order/Order.java
@@ -1,46 +1,18 @@
 package hello.core.order;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class Order {
   private Long memberId;
   private String itemName;
   private int itemPrice;
   private int discountPrice;
 
-  public Order(Long memberId, String itemName, int itemPrice, int discountPrice) {
-    this.memberId = memberId;
-    this.itemName = itemName;
-    this.itemPrice = itemPrice;
-    this.discountPrice = discountPrice;
-  }
-
   public int calculatePrice() {
     return itemPrice - discountPrice;
-  }
-
-  public Long getMemberId() {
-    return memberId;
-  }
-  public String getItemName() {
-    return itemName;
-  }
-  public int getItemPrice() {
-    return itemPrice;
-  }
-  public int getDiscountPrice() {
-    return discountPrice;
-  }
-  
-  public void setMemberId(Long memberId) {
-    this.memberId = memberId;
-  }
-  public void setItemName(String itemName) {
-    this.itemName = itemName;
-  }
-  public void setItemPrice(int itemPrice) {
-    this.itemPrice = itemPrice;
-  }
-  public void setDiscountPrice(int discountPrice) {
-    this.discountPrice = discountPrice;
   }
 
   public String toString() {
