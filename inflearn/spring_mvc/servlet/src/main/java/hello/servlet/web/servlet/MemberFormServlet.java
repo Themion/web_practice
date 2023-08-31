8a5fad515ffb09055e473fa58b8604a10f506953
Themion <themion@naver.com>
2023-08-31 17:28:35 +0900

    Spring MVC - 핸들러 매핑과 핸들러 어댑터

--- inflearn/spring_mvc/servlet/src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
+++ inflearn/spring_mvc/servlet/src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
@@ -9,11 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import hello.servlet.domain.member.MemberRepository;
-
 @WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
 public class MemberFormServlet extends HttpServlet {
-  private MemberRepository memberRepository = MemberRepository.getInstance();
 
   @Override
   protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
