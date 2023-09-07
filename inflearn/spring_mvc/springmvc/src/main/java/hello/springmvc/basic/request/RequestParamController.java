7962763e767cebc10954c594f1e873865f81c940
Themion <themion@naver.com>
2023-09-07 22:53:06 +0900

    HTTP 요청 파라미터 - @RequestParam

--- inflearn/spring_mvc/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ inflearn/spring_mvc/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,25 +1,85 @@
 package hello.springmvc.basic.request;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RestController
+@Controller
 public class RequestParamController {
   @RequestMapping("/request-param-v1")
-  public void requestparamV1(HttpServletRequest req, HttpServletResponse res) throws IOException {
+  public void requestParamV1(HttpServletRequest req, HttpServletResponse res) throws IOException {
     String username = req.getParameter("username");
     int age = Integer.parseInt(req.getParameter("age"));
 
     log.info("username={} age={}", username, age);
 
     res.getWriter().write("ok");
+  }
+
+  @ResponseBody
+  @RequestMapping("/request-param-v2")
+  public String requestParamV2(
+      @RequestParam("username") String memberName,
+      @RequestParam("age") int memberAge) {
+    log.info("username: {}, age: {}", memberName, memberAge);
+
+    return "ok";
+  }
+
+  @ResponseBody
+  @RequestMapping("/request-param-v3")
+  public String requestParamV3(
+      @RequestParam String username,
+      @RequestParam int age) {
+    log.info("username: {}, age: {}", username, age);
+
+    return "ok";
+  }
+
+  @ResponseBody
+  @RequestMapping("/request-param-v4")
+  public String requestParamV4(String username, int age) {
+    log.info("username: {}, age: {}", username, age);
+
+    return "ok";
+  }
+
+  @ResponseBody
+  @RequestMapping("/request-param-required")
+  public String requestParamRequired(
+      @RequestParam(required = true) String username,
+      @RequestParam(required = false) Integer age) {
+    log.info("username: {}, age: {}", username, age);
+
+    return "ok";
+  }
+
+  @ResponseBody
+  @RequestMapping("/request-param-default")
+  public String requestParamDefault(
+      @RequestParam(defaultValue = "default") String username,
+      @RequestParam(defaultValue = "-1") Integer age) {
+    log.info("username: {}, age: {}", username, age);
+
+    return "ok";
+  }
+
+  @ResponseBody
+  @RequestMapping("/request-param-map")
+  public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+    Object username = paramMap.get("username");
+    Object age = paramMap.get("age");
+    log.info("username: {}, age: {}", username, age);
+    return "ok";
   }
 }
