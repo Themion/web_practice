bdd7e65d42fe8657eed41a4d361a27b4addb1feb
Themion <themion@naver.com>
2023-09-14 15:47:12 +0900

    Spring MVC 2 - 타임리프 - 스프링 통합과 폼 - 프로젝트 설정

---
+++ inflearn/spring_mvc_2/item-service/src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -0,0 +1,62 @@
+package hello.itemservice.domain.item;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+public class ItemRepositoryTest {
+  ItemRepository itemRepository = new ItemRepository();
+
+  @AfterEach
+  void afterEach() {
+    itemRepository.clearStore();
+  }
+
+  @Test
+  void save() {
+    // given
+    Item item = new Item("itemA", 10000, 10);
+
+    // when
+    Item savedItem = itemRepository.save(item);
+
+    // then
+    Item foundItem = itemRepository.findById(item.getId());
+    assertThat(savedItem).isEqualTo(foundItem);
+  }
+
+  @Test
+  void findAll() {
+    // given
+    Item itemA = new Item("itemA", 10000, 10);
+    Item itemB = new Item("itemB", 20000, 20);
+    itemRepository.save(itemA);
+    itemRepository.save(itemB);
+
+    // when
+    List<Item> foundList = itemRepository.findAll();
+
+    // then
+    assertThat(foundList.size()).isEqualTo(2);
+    assertThat(foundList).contains(itemA, itemB);
+  }
+
+  @Test
+  void updateItem() {
+    // given
+    Item item = new Item("itemA", 10000, 10);
+    Item updateParam = new Item("itemB", 20000, 20);
+    itemRepository.save(item);
+
+    // when
+    itemRepository.update(item.getId(), updateParam);
+
+    // then
+    assertThat(item.getItemName()).isEqualTo(updateParam.getItemName());
+    assertThat(item.getPrice()).isEqualTo(updateParam.getPrice());
+    assertThat(item.getQuantity()).isEqualTo(updateParam.getQuantity());
+  }
+}
