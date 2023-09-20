bcd0679fd5481a42e3921a9269d373b9fd642c9f
Themion <themion@naver.com>
2023-09-20 17:30:24 +0900

    Spring MVC 2 - 타임리프 - 셀렉트 박스

--- inflearn/spring_mvc_2/item-service-2/src/main/java/hello/itemservice2/web/basic/BasicItemController.java
+++ inflearn/spring_mvc_2/item-service-2/src/main/java/hello/itemservice2/web/basic/BasicItemController.java
@@ -1,5 +1,6 @@
 package hello.itemservice2.web.basic;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import hello.itemservice2.domain.item.DeliveryCode;
 import hello.itemservice2.domain.item.Item;
 import hello.itemservice2.domain.item.ItemRepository;
 import hello.itemservice2.domain.item.ItemType;
@@ -38,6 +40,15 @@ public class BasicItemController {
   @ModelAttribute("itemTypes")
   public ItemType[] itemTypes() {
     return ItemType.values();
+  }
+
+  @ModelAttribute("deliveryCodes")
+  public List<DeliveryCode> deliveryCodes() {
+    List<DeliveryCode> deliveryCodes = new ArrayList<>();
+    deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+    deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+    deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+    return deliveryCodes;
   }
 
   /*
