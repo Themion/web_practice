90b5c9ba83ddddb59e2649d007dea4970c1d9067
Themion <themion@naver.com>
2023-10-15 23:50:50 +0900

    React Native 101 - #2.4 Layout System

--- NomadCoder/react_native/myWeather/App.js
+++ NomadCoder/react_native/myWeather/App.js
@@ -1,26 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="light" />
+    <View style={{ flex: 1 }}>
+      <View style={{ ...styles.tomato, flex: 1 }}></View>
+      <View style={{ ...styles.teal, flex: 3 }}></View>
+      <View style={{ ...styles.orange, flex: 2 }}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#333",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 28,
-    color: "#ddd",
-  },
+  tomato: { backgroundColor: "tomato" },
+  teal: { backgroundColor: "teal" },
+  orange: { backgroundColor: "orange" },
 });
