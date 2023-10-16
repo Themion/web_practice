a5708a26826573b24ecbdf44bc00fe68c9b8c834
Themion <themion@naver.com>
2023-10-16 15:15:15 +0900

    Spring MVC 2 - 검증1 - Validation - BindingResult1

--- inflearn/spring_mvc_2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ inflearn/spring_mvc_2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -1,19 +1,24 @@
 package hello.itemservice.web.validation;
 
-import hello.itemservice.domain.item.Item;
-import hello.itemservice.domain.item.ItemRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
@@ -23,28 +28,27 @@ public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
 
-    private Map<String, String> validateItem(Item item) {
-        Map<String, String> errors = new HashMap<>();
-
+    private BindingResult validateItem(Item item, BindingResult bindingResult) {
         log.info(item.toString());
 
         if (!StringUtils.hasText(item.getItemName()))
-            errors.put("itemName", "상품 이름은 필수입니다.");
+            bindingResult.addError(new FieldError("item", "itemName", "상품 이름은 필수입니다."));
 
         if (item.getPrice() == null || item.getPrice() < 1_000 || item.getPrice() > 1_000_000)
-            errors.put("price", "가격은 1,000 ~ 1,000,000 까지 허용합니다.");
+            bindingResult.addError(new FieldError("item", "price", "가격은 1,000 ~ 1,000,000 까지 허용합니다."));
 
         if (item.getQuantity() == null || item.getQuantity() >= 9_999)
-            errors.put("quantity", "수량은 최대 9,999까지 허용합니다.");
+            bindingResult.addError(new FieldError("item", "quantity", "수량은 최대 9,999까지 허용합니다."));
 
         if (item.getPrice() != null && item.getQuantity() != null) {
             int totalPrice = item.getPrice() * item.getQuantity();
             if (totalPrice < 10_000)
-                errors.put("globalError", "가격 * 수량의 합은 10,000 이상이어야 합니다. 현재 값 = " + totalPrice);
+                bindingResult.addError(
+                        new ObjectError("item", "가격 * 수량의 합은 10,000 이상이어야 합니다. 현재 값 = " + totalPrice));
         }
-        log.info("errors = {}", errors);
+        log.info("bindingResult = {}", bindingResult);
 
-        return errors;
+        return bindingResult;
     }
 
     @GetMapping
@@ -68,12 +72,12 @@ public class ValidationItemControllerV2 {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
-        Map<String, String> errors = validateItem(item);
-        if (!errors.isEmpty()) {
-            model.addAttribute("errors", errors);
+    public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult,
+            RedirectAttributes redirectAttributes,
+            Model model) {
+        bindingResult = validateItem(item, bindingResult);
+        if (bindingResult.hasErrors())
             return "validation/v2/addForm";
-        }
 
         Item savedItem = itemRepository.save(item);
 
@@ -90,12 +94,11 @@ public class ValidationItemControllerV2 {
     }
 
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @ModelAttribute Item item, Model model) {
-        Map<String, String> errors = validateItem(item);
-        if (!errors.isEmpty()) {
-            model.addAttribute("errors", errors);
+    public String editV1(@PathVariable Long itemId, @ModelAttribute Item item, BindingResult bindingResult,
+            Model model) {
+        bindingResult = validateItem(item, bindingResult);
+        if (bindingResult.hasErrors())
             return "validation/v2/editForm";
-        }
 
         itemRepository.update(itemId, item);
         return "redirect:/validation/v2/items/{itemId}";
