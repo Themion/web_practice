e2e37a2477550edfee1eec91dbeb6f14b62d189d
Themion <themion@naver.com>
2024-08-03 21:42:21 +0900

    NextJS for beginner - #2.5 'use client'

--- NomadCoder/nextjs-for-beginners/app/page.tsx
+++ NomadCoder/nextjs-for-beginners/app/page.tsx
@@ -1,10 +1,14 @@
+// import { useState } from "react";
 import Navigation from "../components/navigation";
 
 const App = () => {
+  // This will not work: RSC will not be hydrated!
+  // const [count, setCount] = useState(0);
   return (
     <>
       <Navigation />
       <h1>Hello Next.js!</h1>
+      {/* <button onClick={() => setCount((c) => c + 1)}>{count}</button> */}
     </>
   );
 };
