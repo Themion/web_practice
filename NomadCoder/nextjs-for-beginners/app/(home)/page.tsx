2b32592ee13f6a159bada5885fb6ceeea0b86614
Themion <themion@naver.com>
2024-08-03 23:27:08 +0900

    NextJS for beginner - #3.2 Server Side

--- NomadCoder/nextjs-for-beginners/app/(home)/page.tsx
+++ NomadCoder/nextjs-for-beginners/app/(home)/page.tsx
@@ -1,18 +1,35 @@
 // import { useState } from "react";\
 
+type Movie = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_langage: string;
+  original_title: string;
+  overview: string;
+  popularity: string;
+  poster_path: string;
+  title: string;
+  video: boolean;
+  vote_average: string;
+  vote_count: string;
+};
+
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
 export const metadata = {
   title: "Home",
 };
 
-const App = () => {
-  // This will not work: RSC will not be hydrated!
-  // const [count, setCount] = useState(0);
-  return (
-    <>
-      <h1>Hello Next.js!</h1>
-      {/* <button onClick={() => setCount((c) => c + 1)}>{count}</button> */}
-    </>
-  );
+const getMovies = async (): Promise<Movie[]> => {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  return fetch(URL).then((res) => res.json());
+};
+
+const App = async () => {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 };
 
 export default App;
